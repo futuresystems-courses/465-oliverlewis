75e9b5f5996b664c3bebc7280e90c43bb2885335
oliverlewis <oliverlewis7@gmail.com>
2015-04-16 16:43:10 +0000

    output txt file

--- hw3-cloudmesh/oliveral_cloudmesh_ex3.py
+++ hw3-cloudmesh/oliveral_cloudmesh_ex3.py
@@ -4,8 +4,8 @@ import cloudmesh
 def start_vm():
 	print cloudmesh.shell("vm start --cloud=india --image=futuresystems/ubuntu-14.04 --flavor=m1.small --name={0}".format("oliveral-supervisor3"))
 
-def terminate_vm():
-	cloudmesh.shell("vm delete --group={0} --cloud=india --force".format("oliveral-supervisor2"))
+def stop_vm(name):
+	cloudmesh.shell("vm delete {0} --cloud=india --force".format(name))
 
 
 decision=raw_input('Enter \'start\' or \'stop\' to start/stop VM : ')
@@ -13,6 +13,7 @@ decision=raw_input('Enter \'start\' or \'stop\' to start/stop VM : ')
 if (decision.lower() == "start"):
 	start_vm()
 elif(decision.lower() == "stop"):
-	stop_vm()
+	name = raw_input("Enter vm name to stop: ")
+	stop_vm(name)
 else:
 	print "Enter correct option: start/stop."
